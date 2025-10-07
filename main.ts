3a50eaec73c6f0133cd861da85fbafa1aba17d01
IQ1000jae <115918585+IQ1000jae@users.noreply.github.com>
2025-10-07 19:20:46 +0900

    .

--- main.ts
+++ main.ts
@@ -5,12 +5,16 @@ for (let index = 0; index <= 100; index++) {
 basic.pause(500)
 OLED.clear()
 basic.pause(500)
+OLED.writeStringNewLine("Waiting...")
 basic.forever(function () {
-    basic.pause(5000)
     if (pins.analogReadPin(AnalogReadWritePin.P1) < 20) {
+        OLED.clear()
+        OLED.writeStringNewLine("Working...")
         pins.digitalWritePin(DigitalPin.P12, 1)
         basic.pause(3000)
         pins.digitalWritePin(DigitalPin.P12, 0)
         basic.pause(3000)
+        OLED.clear()
+        OLED.writeStringNewLine("Waiting...")
     }
 })
