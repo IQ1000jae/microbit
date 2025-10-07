5f17ea8cc060ce1051a4fd73d6af58131409fe83
IQ1000jae <115918585+IQ1000jae@users.noreply.github.com>
2025-10-07 19:43:13 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,20 +1,35 @@
-OLED.init(128, 64)
-for (let index = 0; index <= 100; index++) {
-    OLED.drawLoading(index)
-}
+OLED12864_I2C.init(60)
 basic.pause(500)
-OLED.clear()
+OLED12864_I2C.clear()
 basic.pause(500)
-OLED.writeStringNewLine("Waiting...")
+OLED12864_I2C.showString(
+2,
+1.5,
+"Waiting...",
+1
+)
+basic.forever(function () {
+	
+})
 basic.forever(function () {
     if (pins.analogReadPin(AnalogReadWritePin.P1) < 20) {
-        OLED.clear()
-        OLED.writeStringNewLine("Working...")
+        OLED12864_I2C.clear()
+        OLED12864_I2C.showString(
+        2,
+        1.5,
+        "Working...",
+        1
+        )
         pins.digitalWritePin(DigitalPin.P12, 1)
         basic.pause(3000)
         pins.digitalWritePin(DigitalPin.P12, 0)
         basic.pause(3000)
-        OLED.clear()
-        OLED.writeStringNewLine("Waiting...")
+        OLED12864_I2C.clear()
+        OLED12864_I2C.showString(
+        2,
+        1.5,
+        "Waiting...",
+        1
+        )
     }
 })
